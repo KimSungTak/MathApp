8facab58319d763b91d268d8a5d17d5e567cb5f1
Charlie <cmilius204@gmail.com>
2015-04-23 14:30:00 -0500

    Built the shell for the division game.  Initial commit

---
+++ app/src/main/java/com/toomanycooksapp/mathapp/DrawDivisionGame.java
@@ -0,0 +1,24 @@
+package com.toomanycooksapp.mathapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Charlie Milius on 4/23/2015.
+ * Creates view and runs the division game
+ */
+public class DrawDivisionGame extends View {
+
+    //default constructor
+    public DrawDivisionGame(Context context) {
+        super(context);
+    }
+
+
+}
